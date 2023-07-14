3a6a01b96eaa791c476322fa9f37d64afca229c7
kath3rine <not.katherine.li@gmail.com>
2023-07-14 19:31:44 -0400

    track info

--- match.py
+++ match.py
@@ -3,6 +3,7 @@ import requests
 CLIENT_ID='f1b645b681544d408913e0f55784b3a6'
 CLIENT_SECRET='a2c5b97a944647d8bce6722c772adbc8'
 REDIRECT_URI='http://127.0.0.1:5000/'
+BASE_URL = 'https://api.spotify.com/v1/'
 
 def authorize():
 	auth_response = requests.post('https://accounts.spotify.com/api/token', {
@@ -13,3 +14,17 @@ def authorize():
 	auth_response_data = auth_response.json()
 	return auth_response_data['access_token']
 
+access_token = authorize()
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+
+def get_track(playlist):
+	r = requests.get(BASE_URL + 'playlists/' + playlist + '/tracks', headers=headers )
+	r = r.json()
+	return r['public']
+
+def track_info(track_id):
+	r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
+	r = r.json()
+	return r
