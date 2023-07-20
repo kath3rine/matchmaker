951cbbfc756282c03fc21ea99e29877d70fd3769
kath3rine <not.katherine.li@gmail.com>
2023-07-20 19:50:35 -0400

    print match result on webpage!!

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 from flask import Flask, request, render_template
-from match import get_data, get_image, recommend, artist_ids, find_shared, artist_info, genres
+from match import matchmaker
 
 app = Flask(__name__)
-
-# https://open.spotify.com/user/kli-17?si=dc7771a37b7c480f
-# https://open.spotify.com/playlist/2b08l28qJ61zKFztCRrNOl?si=dbc23b7bc7d3480b
-# https://open.spotify.com/playlist/6aTmvWaCEYUOy9xOH5aQ6I?si=30540fbdf2b6479e
-# https://open.spotify.com/playlist/5t1y9F77hIHGFIUJbdy2PH?si=468e510d221749d8
 
 matches = []
 
@@ -18,50 +13,15 @@ def home():
 def match():
   
   # get urls/ids
-  uid1, uid2 = request.form['user1'], request.form['user2']
-  pid1, pid2 = request.form['playlist1'], request.form['playlist2']
-  pid1, pid2 = pid1[34 : -20], pid2[34 : -20]
+  pid1y = request.form['input1']
+  pid1x =  request.form['input2']
+  pid2 = request.form['input3']
+  uid = request.form['input4']
 
-  # create jsons of data
-  user1, user2 = get_data(uid1, 'users'), get_data(uid2, 'users') 
-  playlist1, playlist2 = get_data(pid1, 'playlists'), get_data(pid2, 'playlists')
-
-  # get artists and genres
-  a1, a2 = artist_ids(playlist1), artist_ids(playlist2) 
-  a = find_shared(a1, a2) 
-  g1, g2 = genres(a1), genres(a2)
-  g = find_shared(g1, g2)
-
-  ##### PROFILE PAGE #####
-  # HEADER
-  pfp = get_image(user2)
-  name = user2['display_name']
-  compatibility = '69'# filler value
-
-  # ABOUT
-  if len(a) != 0: # seed = shared artists
-    recs = recommend(a, 'artists')
-  elif len(g) != 0: # seed = shared genres
-    recs = recommend(g, 'genres')
-  else: # seed = first 2 artists from each playlist
-    temp = [a1[0], a1[1], a2[0], a2[1]]
-    recs = recommend(temp, 'artists')
-
-
-  # PROMPTS
-  s_artists = 'none :(' if len(a) == 0 else str(artist_info(a, 'name')[ : 5])[1 : -1]
-  s_genres = 'none :(' if len(g) == 0 else str(g[ : 5])[1 : -1]
+  out = matchmaker(pid1y, pid1x, pid2, uid)
   
-  # BUBBLES
-  f_artists = artist_info(a2, 'name')[ : 5]
-  f_genres = g2[ : 5]
-
   # render match page
-  return render_template('match.html', pfp=pfp, name=name, compatibility=compatibility, 
-    recs=recs, len_recs = len(recs), 
-    s_artists=s_artists, s_genres=s_genres,
-    f_artists=f_artists, len_f_artists=len(f_artists),
-    f_genres=f_genres, len_f_genres = len(f_genres))
+  return render_template('match.html', out=out)
 
 @app.route('/like', methods=['GET', 'POST'])
 def like():
--- match.py
+++ match.py
@@ -1,118 +1,76 @@
-import requests
-from config import CLIENT_ID, CLIENT_SECRET
-
-REDIRECT_URI='http://127.0.0.1:5000/'
-BASE_URL = 'https://api.spotify.com/v1/'
-
-def authorize():
-	auth_response = requests.post('https://accounts.spotify.com/api/token', {
-		'grant_type': 'client_credentials',
-		'client_id': CLIENT_ID,
-		'client_secret': CLIENT_SECRET,
-	})
-	auth_response_data = auth_response.json()
-	return auth_response_data['access_token']
-
-access_token = authorize()
-headers = {
-    'Authorization': 'Bearer {token}'.format(token=access_token)
-}
-
-# PARAMS item_id: id of object, item_type: type of object (artist, playlist, etc)
-# RETURNS json file of object's data
-def get_data(item_id, item_type):
-	data = requests.get(BASE_URL + item_type + '/' + item_id, headers=headers )
-	return data.json()
-
-# PARAMS data: json of all playlist data, key: target feature
-# RETURNS list of track info
-def get_track_info(data, key):
-	r = []
-	tracks = data['tracks']
-	for track in tracks['items']:
-		r.append(track['track'][key])
-	return r
-
-# PARAM list of track ids
-# RETURNS list of dicts containing audio features (1 elem / song)
-def get_audio_features(track_ids):
-	r = []
-	for track in track_ids:
-		data = get_data(track, 'audio-features')
-		r.append(data)
-	return r
+from helpers import get_image, get_artist_info, get_data, get_genres, create_df, find_compatibility, find_top, find_shared, find_features, recommend_artists, recommend_tracks, combine_df
 
 
-# PARAM list of track ids
-# RETURNS list of artists' ids
-def get_artist_info(data, key):
-	r = []
-	tracks = data['tracks']
-	for track in tracks['items']:
-		r.append(track['track']['artists'][0][key])
-	return r
+def matchmaker(pid1y, pid1x, pid2, uid):
+    # PARAMS 
+    # pid1, pid1x, pid2: playlist urls of songs user1 likes, user1 dislikes, and user2 likes, respectively
+    # uid: user2's id
 
-# PARAMS ids: list of ids (artist, track etc), key: target features
-# RETURNS list of target feature values
-def get_value(ids, key):
-	r = []
-	for i in ids:
-		r.append(i[key])
-	return r
+    # RETURNS
+    # dict: keys = variables/features, values = value
 
-# PARAMS item_id: id, item_type: user, album, etc
-# RETURNS link to image
-def get_image(data):
-	images = data['images']
-	image = images[0]# first obj in ImageObject array
-	return image['url']
+    # constants
+    REDUCED_FEATURES = ['danceability', 'energy', 'acousticness', 'valence', 'loudness', 'tempo', 'liveness', 'instrumentalness', 'mode']
+    VERY_REDUCED_FEATURES = ['danceability', 'energy', 'acousticness', 'valence', 'liveness', 'instrumentalness']
 
-# PARAM list of artist ids
-# RETURN list of genres (str)
-def get_genres(artist_ids):
-	r = []
-	for i in artist_ids:
-		data = get_data(i, 'artists')
-		genres = data['genres']
-		if genres[0] not in r:
-			r.append(genres[0])
-	return r
+    # initialize dictionary of features to return
+    var_list = ['pfp', 'name', 'compatibility', # header
+                'sim_features', # prompts
+                's_artists', 's_genres', 'f_artists', 'f_genres', # bubbles
+                'rec_tracks', 'rec_artists', # about
+            ]
 
-# PARAMS ids: list of ids (artist or genre), mode: artist or genre
-# RETURNS: list of 5 songs, format "[title] by [artist]"
-def recommend_tracks(ids, mode):
-	seed = '='
-	for i in ids:
-		seed += (i + '%2C') # add item + comma
-	seed = seed[: -3] # remove the last comma'
+    # keys = variables, values = value for html page
+    r = dict(zip(var_list, [None] * len(var_list))) 
 
-	data = requests.get(BASE_URL + 'recommendations?limit=3&seed_' + mode + seed, headers=headers)
-	data = data.json()
+    # splice pid from url
+    pid1y, pid1x = pid1y[34 : -20], pid1x[34 : -20] 
+    pid2 = pid2[34 : -20] 
 
-	r = []
-	for track in data['tracks']:
-		temp = track['name'] + " by " + track['artists'][0]['name']
-		r.append(temp)
+    # create dataframes 
+    df1y, df1x = create_df(pid1y, 1), create_df(pid1x, 0) # user1 likes, user1 dislikes
+    df1 = combine_df([df1y, df1x]) # all user1
+    df2 = create_df(pid2, 1) # user2
 
-	return r
+    user_data = get_data(uid, 'users')
 
-def recommend_artists(artist_ids):
-	r = []
-	for artist in artist_ids:
-		data = requests.get(BASE_URL + 'artists/' + artist + '/related-artists')
-		data = data.json()
-		a = data['artists'][0]
-		if a['name'] not in r:
-			r.append(a['name'])
-	return
+    # artists, genres, features
+    a1, a2 = df1y['artist_ids'].tolist(), df2['artist_ids'].tolist() # artists per person, w dupes (ids)
+    top_a1 = find_top(a1, 1) # user1's top 2 artists (ids)
+    top_a2 = find_top(a2, 3) # user2's top 3 artists (ids)
+    a = find_shared([a1, a2])[ : 5] # top 5 shared artists (ids)
+
+    g1, g2 = get_genres(a1), get_genres(a2) # genres w/ dupes (strs)
+    g = find_shared([g1, g2])[ : 5] # top 5 shared genres (strs)
+
+    ###### PROFILE PAGE ######
+
+    # HEADER
+    r['pfp'] = get_image(user_data)
+    r['name'] = user_data['display_name']
+    r['compatibility'] = find_compatibility(df1[REDUCED_FEATURES], df1['likes'], df2[REDUCED_FEATURES], df2['likes']) * 100
+
+    # PROMPTS 
+    r['sim_features'] = find_features(df1y, df2, VERY_REDUCED_FEATURES)
+
+    # BUBBLES (shared + favs)
+    r['s_artists'] = get_artist_info(a, 'name')
+    r['s_genres'] = g
+    r['f_artists'] = get_artist_info(top_a2, 'name')[ : 3]
+    r['f_genres'] = [*set(g2)][ : 3]
+
+    # ABOUT (RECS)
+    if len(a) != 0: # seed = shared artists
+        r['rec_tracks'] = recommend_tracks(a, 'artists')
+        r['rec_artists'] = recommend_artists(a)
+    else: # no shared artists
+        temp = [top_a1[0], top_a2[0], top_a2[1]] # top 2 artists per person
+        r['rec_artists'] = recommend_artists(temp)
+        if len(g) != 0: # seed = shared genres
+            r['rec_tracks'] = recommend_tracks(g, 'genres')
+        else: # seed = top 2 artists per person
+            r['rec_tracks'] = recommend_tracks(temp, 'artists')
+    return r
 
 
-# PARAMS list1, list2: data to be compared
-# RETURNS set of items both playlists have in common
-def find_shared(list1, list2):
-	r = []
-	for i in list1:
-		if i in list2 and i not in r:
-			r.append(i)
-	return r
 
